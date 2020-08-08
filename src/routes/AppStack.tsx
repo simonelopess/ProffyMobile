e227bf9086c5391c27f4d5d831466fdbf2262731
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-08 18:58:23 -0300

    personalização das tabs e criado o header page

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -5,6 +5,7 @@ const { Navigator, Screen } = createStackNavigator();
 
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from './StudyTabs';
 
 function AppStack() {
   return (
@@ -12,6 +13,7 @@ function AppStack() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
+        <Screen name="Study" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
   );
